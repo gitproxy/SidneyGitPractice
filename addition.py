9ebf3364894b90d3a145ba0b8b0093f7e13d6d25
Sidney2408 <sidney_suen@mymail.sutd.edu.sg>
2016-12-29 00:03:47 +0800

    pinardy stuff

---
+++ addition.py
@@ -0,0 +1,11 @@
+def addition(a,b):
+	return a + b
+
+#Sidney was here, blargfarx
+#Pinardy entered
+#Sidney made a change!
+
+#If you see this message, that means the branch was created successfully
+
+print addition(1,2)
+#Post merge with sidney branch
